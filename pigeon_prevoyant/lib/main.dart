17eb80ed271d83e6f1cae6942e5cfcfccbf8e6b2
Walid SADAOUI <walidsadaoui@hotmail.fr>
2024-03-17 18:13:57 +0100

    init and store budget in local storage

--- pigeon_prevoyant/lib/main.dart
+++ pigeon_prevoyant/lib/main.dart
@@ -1,64 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PigeonPrevoyant());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PigeonPrevoyant extends StatelessWidget {
+  const PigeonPrevoyant({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pigeon Prévoyant',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         fontFamily: 'RobotoFlex',
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Pigeon Prévoyant'),
+      home: const InitBudgetPage(title: 'Pigeon Prévoyant'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class InitBudgetPage extends StatefulWidget {
+  const InitBudgetPage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<InitBudgetPage> createState() => _InitBudgetPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-    // Create a text controller and use it to retrieve the current value
-  // of the TextField.
+class _InitBudgetPageState extends State<InitBudgetPage> {
   final myController = TextEditingController();
+  double? _budget;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadBudget();
+  }
 
   @override
   void dispose() {
@@ -67,45 +48,43 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
   }
 
+  /// Load the initial budget value from persistent storage on start,
+  /// or fallback to 0 if it doesn't exist.
+  Future<void> _loadBudget() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _budget = prefs.getDouble('budget');
+    });
+  }
+
+  /// After a click, increment the budget state and
+  /// asynchronously save it to persistent storage.
+  Future<void> _incrementBudget() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _budget = double.parse(myController.text);
+      if (_budget != null) {
+        prefs.setDouble('budget', _budget as double);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: _budget == null ? <Widget>[
             TextField(
               controller: myController,
+              keyboardType: TextInputType.number,
+              autofocus: true,
+              inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'^[0-9]+.?[0-9]*'))],
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Votre budget initial'
@@ -113,23 +92,48 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             ElevatedButton(
               onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                // If the form is valid, display a snackbar. In the real world,
-                // you'd often call a server or save the information in a database.
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(myController.text)),
-                );
+                _incrementBudget();
+                  // Navigator.push(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const SecondRoute()),
+                  // );
               },
               child: const Text("C'est parti !"),
+            ),
+          ] : 
+          <Widget>[
+            Text(
+              'Solde : $_budget €'
             ),
           ],
         ),
       ),
       // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
+      //   onPressed: _incrementBudget,
       //   tooltip: 'Increment',
       //   child: const Icon(Icons.add),
       // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
     );
   }
 }
